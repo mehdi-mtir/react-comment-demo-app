d0fdb0c1e0caa0866c949c05bb55b4fef91e3bf3
mehdi-mtir <mehdi.mtir@gmail.com>
2025-01-07 12:24:21 +0100

    approuved comments

--- src/App.js
+++ src/App.js
@@ -41,10 +41,10 @@ function App() {
   //comments est désormais une variable d'état
   //comments est une variable immutable
   const [comments, setComments] = useState([
-    {id: 1,name: "Matt", date: "Today at 5:42PM", text: "How artistic!", avatar: "https://semantic-ui.com/images/avatar/small/matt.jpg", approuved : "pending"},
-    {id: 2,name: "Elliot Fu", date: "Yesterday at 12:30AM", text: "This has been very useful for my research. Thanks as well!",  avatar: "https://semantic-ui.com/images/avatar/small/elliot.jpg", approuved : "pending"},
-    {id: 3,name: "Jenny Hess", date: "Just now", text: "Elliot you are always so right :)", avatar: "https://semantic-ui.com/images/avatar/small/jenny.jpg", approuved : "pending"},
-    {id: 4,name: "Joe Henderson", date: "5 days ago", text: "Dude, this is awesome. Thanks so much", avatar: "https://semantic-ui.com/images/avatar/small/joe.jpg", approuved : "pending"},
+    {id: 1,name: "Matt", date: "Today at 5:42PM", text: "How artistic!", avatar: "https://semantic-ui.com/images/avatar/small/matt.jpg", isApprouved : false},
+    {id: 2,name: "Elliot Fu", date: "Yesterday at 12:30AM", text: "This has been very useful for my research. Thanks as well!",  avatar: "https://semantic-ui.com/images/avatar/small/elliot.jpg", isApprouved : false},
+    {id: 3,name: "Jenny Hess", date: "Just now", text: "Elliot you are always so right :)", avatar: "https://semantic-ui.com/images/avatar/small/jenny.jpg", isApprouved : false},
+    {id: 4,name: "Joe Henderson", date: "5 days ago", text: "Dude, this is awesome. Thanks so much", avatar: "https://semantic-ui.com/images/avatar/small/joe.jpg", isApprouved : false},
   ])
 
   const declineComment = (id) => {
@@ -52,13 +52,36 @@ function App() {
     console.log(comments);
   } 
 
+  /*const approveComment = (id) => {
+    const newComments = comments.map(comment => {
+      if(comment.id === id){
+        return {...comment, isApprouved : true}
+      }
+      else{
+        return comment;
+      }
+    } )
+    setComments(newComments);
+  }*/
+
+  const approveComment = (id) => {
+    setComments(comments.map(comment => comment.id === id ? {...comment, isApprouved : true} : comment));
+  }
+
+
+
   return (
     <>
       <h1>First React App</h1>
         <div className="ui cards"> 
             {
               comments.map(
-                (comment)=><Card key={comment.id} id={comment.id} declineCommentHandler={declineComment}>
+                (comment)=><Card 
+                  key={comment.id} 
+                  id={comment.id}
+                  isApprouved={comment.isApprouved} 
+                  declineCommentHandler={declineComment}
+                  approveCommentHandler={approveComment}>
                     <Comment {...comment} />
                   </Card>
               )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,10 +5,12 @@ function Card(props){
                 {props.children}
             </div>
             <div className="extra content">
-            <div className="ui two buttons">
-                <div className="ui basic green button" onClick={()=>console.log("commentaire" + props.id + " approuvé")}>Approve</div>
-                <div className="ui basic red button" onClick={()=>props.declineCommentHandler(props.id)}>Decline</div>
-            </div>
+            {
+                (!props.isApprouved) && <div className="ui two buttons">
+                    <div className="ui basic green button" onClick={()=>props.approveCommentHandler(props.id)}>Approve</div>
+                    <div className="ui basic red button" onClick={()=>props.declineCommentHandler(props.id)}>Decline</div>
+                </div>
+            }
             </div>
         </div>
     )
